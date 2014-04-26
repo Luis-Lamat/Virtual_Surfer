8d197b038fad177db50c560970dc4c2491df8b28
Enrique Marroquin <kike93_@hotmail.com>
2014-04-26 10:02:46 -0500

    demo game

--- Virtual Surfer/TechnoGameMyScene.m
+++ Virtual Surfer/TechnoGameMyScene.m
@@ -7,43 +7,89 @@
 //
 
 #import "TechnoGameMyScene.h"
-//quibooo vatoo
 
 @implementation TechnoGameMyScene
 
--(id)initWithSize:(CGSize)size {    
+int score = 0;
+
+-(void) createShip {
+    
+    if (score == 20) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"20!"
+                                                        message:@"awesome"
+                                                       delegate:nil
+                                              cancelButtonTitle:@"continue"
+                                              otherButtonTitles:nil];
+        [alert show];
+        
+    }
+    
+    SKSpriteNode *ship = [SKSpriteNode  spriteNodeWithImageNamed:@"Ship"];
+    
+    //Creates a random ship position
+    int maxX = CGRectGetMaxX(self.frame);
+    float ranX = (arc4random()%maxX) + 1;
+    
+    int maxY = CGRectGetMaxY(self.frame);
+    float ranY = (arc4random()%maxY) + 1;
+    
+    ship.position = CGPointMake(ranX, ranY);
+    ship.size     = CGSizeMake(80, 52);
+    ship.name     = @"Ship";
+    
+    [self addChild:ship];
+}
+
+-(id)initWithSize:(CGSize)size {
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
         
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
+        self.backgroundColor = [SKColor colorWithRed:0.204 green:0.596 blue:0.859 alpha:1.0];
         
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+        //Create our score label
+        SKLabelNode *label = [SKLabelNode labelNodeWithFontNamed:@"ArialRoundedMTBold"];
+        label.fontSize = 48;
+        label.text = @"0";
+        label.position = CGPointMake(CGRectGetMidX(self.frame), 40);
+        label.color = [SKColor colorWithRed:1 green:1 blue:1 alpha:1.0];
+        label.name = @"scoreLabel";
         
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
+        [self addChild:label];
         
-        [self addChild:myLabel];
+        [self createShip];
     }
     return self;
+}
+
+-(void)selectNodeForTouch:(CGPoint)touchLocation {
+    //Checks if a ship was touched
+    SKSpriteNode *touchedNode = (SKSpriteNode *)[self nodeAtPoint:touchLocation];
+    if ([[touchedNode name] isEqualToString:@"Ship"]) {
+        SKLabelNode *scoreLabel = (SKLabelNode *)[self childNodeWithName:@"scoreLabel"];
+        score++;
+        scoreLabel.text = [NSString stringWithFormat:@"%d", score];
+        
+        SKSpriteNode *ship = (SKSpriteNode *)[self childNodeWithName:@"Ship"];
+        ship.name = @"DisabledShip";
+        
+        SKAction *grow =  [SKAction scaleTo:1.2 duration:0.1];
+        SKAction *shrink = [SKAction scaleTo:0 duration:0.07];
+        SKAction *removeNode = [SKAction removeFromParent];
+        SKAction *seq = [SKAction sequence:@[grow, shrink, removeNode]];
+        
+        [ship runAction: seq];
+        [self createShip];
+    }
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
     
     for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
+        //CGPoint location = [touch locationInNode:self];
+        UITouch *touch = [touches anyObject];
+        CGPoint positionInScene = [touch locationInNode:self];
+        [self selectNodeForTouch:positionInScene];
     }
 }
 
